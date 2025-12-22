5d62b3d6c7ef6099d0791628b72689f6884ce8af
love-coding-git <jus10gnoh.gmail.com>
2025-12-22 14:56:00 +0000

    form width

--- src/app/(protected)/meus-dados/page.tsx
+++ src/app/(protected)/meus-dados/page.tsx
@@ -515,8 +515,8 @@ export default function MeusDadosPage() {
                       </div>
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                      <div className="md:col-span-2 space-y-2">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                      <div className="space-y-2">
                         <Label htmlFor="address_street">Rua/Avenida</Label>
                         <Input
                           id="address_street"
@@ -893,8 +893,8 @@ export default function MeusDadosPage() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <div className="md:col-span-2 space-y-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="space-y-2">
                 <Label htmlFor="pj_address_street">Rua/Avenida</Label>
                 <Input
                   id="pj_address_street"
